180938936834074de1a6c007ad7c405ce4bea73c
IMS.mobility <ims.mobility@IMSmobilityui-MacBookPro.local>
2025-06-17 21:31:14 +0900

    Initial commit

---
+++ api/src/test/java/org/example/api/service/ApplyServiceTest.java
@@ -0,0 +1,27 @@
+package org.example.api.service;
+
+import org.example.api.repository.CouponRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ApplyServiceTest {
+
+    @Autowired
+    private ApplyService applyService;
+
+    @Autowired
+    private CouponRepository couponRepository;
+
+    @Test
+    void 한번만_응모() {
+        applyService.applyCoupon(1L);
+        long count = couponRepository.count();
+
+        assertEquals(1, count);
+    }
+
+}
