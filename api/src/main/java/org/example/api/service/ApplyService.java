5c32078707975c4642e14841ea0aebb31c8e1291
ironmask431 <ironmask431@gmail.com>
2025-06-25 21:29:53 +0900

    applyService 에 쿠폰발급 메세지 kafka로 보내도록 설정

--- api/src/main/java/org/example/api/service/ApplyService.java
+++ api/src/main/java/org/example/api/service/ApplyService.java
@@ -3,6 +3,7 @@ package org.example.api.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.example.api.domain.Coupon;
+import org.example.api.kafka.producer.CouponCreateProducer;
 import org.example.api.repository.CouponCountRepository;
 import org.example.api.repository.CouponRepository;
 import org.springframework.stereotype.Service;
@@ -13,15 +14,20 @@ import org.springframework.stereotype.Service;
 public class ApplyService {
     private final CouponRepository couponRepository;
     private final CouponCountRepository couponCountRepository;
+    private final CouponCreateProducer couponCreateProducer;
 
     //쿠폰 발급 로직
     public void applyCoupon(Long userId)   {
-        //long count = couponRepository.count();
+//        long count = couponRepository.count(); // db에서 조회하는 방식
+
+        // count를 redis "coupon_count" 에서 조회
+        // 주의 * : 매번 실행 시 마다 redis의 coupon_count 초기화 해줘야함. 명렁어 : flushall
         Long count = couponCountRepository.increase("coupon_count");
         log.info("applyCoupon - count = {}", count);
         if (count > 100) {
             return;
         }
-        couponRepository.save(new Coupon(userId));
+//        couponRepository.save(new Coupon(userId));
+        couponCreateProducer.send(userId); // 쿠폰 생성 요청을 kakfa 로 보내도록 적용.
     }
 }
